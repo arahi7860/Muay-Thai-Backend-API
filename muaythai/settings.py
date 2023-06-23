8a50a0e026dfa7ef9a0b8e6391071d0777a3dacd
Ahmed Rahi <54910341+arahi7860@users.noreply.github.com>
2023-06-23 19:01:22 -0400

    back to old settings.py

--- muaythai/settings.py
+++ muaythai/settings.py
@@ -154,9 +154,3 @@ STATICFILES_DIRS = [
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-REST_FRAMEWORK = {
-        'DEFAULT_PARSER_CLASSES': [
-            'rest_framework.parsers.JSONParser',
-    ],    
-}
