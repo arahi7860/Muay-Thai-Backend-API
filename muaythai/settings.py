e763fa12fd8b021b048322b29aac17331c6bd09d
Ahmed Rahi <54910341+arahi7860@users.noreply.github.com>
2023-06-23 18:30:54 -0400

    change to middleware

--- muaythai/settings.py
+++ muaythai/settings.py
@@ -34,7 +34,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.json.JsonResponseMiddleware',  # Add this line
 ]
 
 ROOT_URLCONF = 'muaythai.urls'
