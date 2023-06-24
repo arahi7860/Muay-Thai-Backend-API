1bf7151abc9dfdd21306e5aebda251b3f6f31543
Ahmed Rahi <54910341+arahi7860@users.noreply.github.com>
2023-06-24 02:55:53 -0400

    changes again

--- muaythai/settings.py
+++ muaythai/settings.py
@@ -155,8 +155,6 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
