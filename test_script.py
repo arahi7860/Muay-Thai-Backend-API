f266cacbbe0bdcf356517fa601ea05840042e316
Ahmed Rahi <54910341+arahi7860@users.noreply.github.com>
2023-06-24 00:57:23 -0400

    test_script.py

---
+++ test_script.py
@@ -0,0 +1,17 @@
+import requests
+
+url = 'https://muay-thai-backend-api-production.up.railway.app/techniques/'
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json'
+}
+data = {
+    "name": "Test Technique 5",
+    "description": "I am tired",
+    "img": "https://example.com/test.gif",
+    "category": "Punches"
+}
+
+response = requests.post(url, json=data, headers=headers)
+print(response.status_code)
+print(response.json())
